715ba1d8a8fd5d35e11016914cdb0d52f9b6c1dc
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2021-06-12 21:47:41 +0900

    fix package name

--- src/main/kotlin/me/yu1ro/minimumgrpckt/Hello.kt
+++ src/main/kotlin/me/yu1ro/minimumgrpckt/Hello.kt
@@ -2,9 +2,6 @@ package me.yu1ro.minimumgrpckt
 
 import io.grpc.Server
 import io.grpc.ServerBuilder
-import me.yu1ro.minimumgrpckt.helloworld.GreeterGrpcKt
-import me.yu1ro.minimumgrpckt.helloworld.HelloReply
-import me.yu1ro.minimumgrpckt.helloworld.HelloRequest
 
 class Hello (private val port: Int) {
     val server: Server = ServerBuilder
